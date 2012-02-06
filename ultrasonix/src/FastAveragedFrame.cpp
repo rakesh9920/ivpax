36d52d0e9fda356bf7e117976c3df3cabd30cca9
Bernie <bsauce@gmail.com>
2012-02-06 12:30:23 -0500

    added fast frame file

---
+++ ultrasonix/src/FastAveragedFrame.cpp
@@ -0,0 +1,76 @@
+/* INCLUDE */
+#include "FrameClasses.h"
+
+/* CONSTRUCTORS & DESTRUCTORS */
+FastAveragedFrame::FastAveragedFrame(texo * _tex, texoTransmitParams * _tx, texoReceiveParams * _rx)
+: Frame(_tex, _tx, _rx) {setNumberOfLinesToAvg(1);}
+
+/* METHODS */
+void FastAveragedFrame::loadTable() {
+
+	if (!tex->beginSequence()) 
+		throw Error("Could not define sequence");
+
+	rx->aperture = 64;
+	tex->addTransmit(*tx);
+
+	for (int channel = 0; channel < 128; channel++) {
+
+		if (channel < 64) 
+			rx->centerElement = 315;
+		else 
+			rx->centerElement = 955;
+
+		int c = channel % 64;
+		rx->channelMask[0] = (c < 32) ? (1 << c) : 0;
+		rx->channelMask[1] = (c >= 32) ? (1 << (c - 32)) : 0;
+		tex->addReceive(*rx);
+	}
+
+	lineCount = 0;
+	for (int channel = 0; channel < 128; channel++) {
+		for (int line = 0; line < numberOfLinesToAvg; line++) {
+
+			tx->tableIndex = 1;
+			rx->tableIndex = channel;
+
+			lineSize = tex->addLine(rfData, *tx, *rx);
+
+			if (lineSize == -1)
+				throw Error("Could not add line to sequence");
+
+			lineCount++;
+		}
+	}
+
+	if (!tex->endSequence()) 
+		throw Error("Could not define sequence");
+
+	if (numberOfLinesToAvg == 1)
+		lineSize -= 4;
+
+	validsequence = true;
+}
+
+unsigned char * FastAveragedFrame::average(signed short * set) {
+
+	for (int channel = 0; channel < 128; channel++) {
+		for (int sample = 0; sample < lineSize/2; sample++) {
+
+			double sum = 0;
+
+			for (int line = 0; line < numberOfLinesToAvg; line++) 
+				sum += ((double) set[channel*numberOfLinesToAvg*lineSize/2 + line*lineSize/2 + sample]);
+
+			set[channel*lineSize/2 + sample] = (signed short) ((((double) digitalGain)*sum)/((double) numberOfLinesToAvg));
+		}
+	}
+
+	return (unsigned char *) set;
+}
+
+void FastAveragedFrame::saveToBuffer(Buffer * buf) {
+
+	average((signed short *) tex->getCineStart(0));
+	buf->transferLine(tex->getCineStart(0), lineSize*128);
+}
