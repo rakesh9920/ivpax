26eb950365f915f6c0d3aa26e201b8f3fc0827ab
Bernie <bsauce@gmail.com>
2012-04-12 18:57:40 -0400

    update

--- mexo/texo_getCineStart.cpp
+++ mexo/texo_getCineStart.cpp
@@ -12,7 +12,7 @@ int nrhs, const mxArray * prhs[]) {
     uint32_t blockid = static_cast <uint32_t> (mxGetScalar(prhs[1]));
     
 	// create plhs
-    uint32_t * start = static_cast <uint32_t *> (tex->getCineStart(blockid));
-    plhs[0] = mxCreateNumericArray(1, 1, mxUINT32_CLASS, mxREAL);
+    uint32_t * start = reinterpret_cast <uint32_t *> (tex->getCineStart(blockid));
+    plhs[0] = mxCreateNumericMatrix(1, 1, mxUINT32_CLASS, mxREAL);
     *((uint32_t *) mxGetData(plhs[0])) = reinterpret_cast <uint32_t> (start);
 }
