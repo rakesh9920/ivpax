5050c3736b36212317a32873f4ed7aeca6b0b15b
Bernie <bsauce@gmail.com>
2013-03-29 18:13:58 -0400

    -

--- imgre/sirm/gfbeamform.m
+++ imgre/sirm/gfbeamform.m
@@ -1,4 +1,5 @@
 function [bfline] = gfbeamform(rxsignals, txpts, rxpts, fldpts)
+% General frequency beamformer (for synthetic RF data)
 
 numfldpts = size(fldpts,2);
 [numsigs siglength numinst] = size(rxsignals);
@@ -18,7 +19,7 @@ padsignals = [frontpad rxsignals backpad];
 
 RXSIGNALS = fft(padsignals, [], 2);
 
-f = sampfreq*linspace(0,1,nfft/2+1);
+f = sampfreq/2*linspace(0,1,nfft/2+1);
 freq = [f(1:end-1) -f(end) -fliplr(f(2:(end-1)))];
 
 bar = upicbar('Beamforming ...');
@@ -27,36 +28,23 @@ for inst = 1:numinst
     RX = squeeze(RXSIGNALS(:,:,inst));
     for fp = 1:numfldpts
         
-        upicbar(bar, inst*fp/(numinst*numfldpts));
+        upicbar(bar, (inst*numfldpts + fp)/(numinst*numfldpts));
         
         dist = totaldist(:, fp);
         delays = -dist./soundspeed;
-        delind = (delays > (siglength/sampfreq)) | (delays < -siglength/sampfreq);
+        delind = (delays > (siglength/sampfreq)) | (delays < -(siglength/sampfreq));
         
         if all(delind)
             continue
         end
-%       
-%         EXP1 = exp(-2*pi*1i.*delays*freq(1:(nfft/2)));
-%         EXP2 = conj(exp(-2*pi*1i.*delays*freq(nfft/2+1)));
-%         EXP3 = conj(exp(-2*pi*1i.*delays*freq(2:(nfft/2))));
-        
-%         BFSIG = [EXP1 EXP2 EXP3].*RX;
+
+        delays(delind) = [];
         BFSIG = exp(-2*pi*1i.*delays*freq).*RX;
         bfsig = real(ifft(BFSIG, [], 2));
-        bfsig(delind,:) = [];
+        %bfsig(delind,:) = [];
         sumsig = sum(bfsig);
         bfline(1, fp, inst) = sumsig(size(frontpad,2)+1);
         
-%         sum = 0;
-%         for d = 1:length(delays)
-%             if delays(d) > size(rxsignals, 2) || delays(d) < 1
-%                 continue
-%             end
-%             sum = sum + rxsignals(d, delays(d), inst);
-%         end
-%         
-%         bfline(1, fp, inst) = sum;
     end
 end
 end
