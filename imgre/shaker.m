be81183be0bc08dd070e121563b9d514243d1d41
Bernie <bsauce@gmail.com>
2013-04-30 18:38:00 -0400

    -

--- imgre/shaker.m
+++ imgre/shaker.m
@@ -1,8 +1,8 @@
 addpath ./sirm/
 addpath ./beamformer/
 
-
-[header, ~] = readDAQ('./10hz/', ones(1,128), 1, true);
+%% read in RAW daq data
+[header, ~] = readDAQ('./rfdata/dataset2/tx10hz/', ones(1,128), 1, true);
 
 nChannel = header(1) + 1;
 nFrame = 100;%header(2);
@@ -11,14 +11,30 @@ nSample = header(3);
 Rfc = zeros(nChannel, nSample, nFrame, 'int16');
 
 for fr = 1:nFrame
-   [~, rf] = readDAQ('./10hz/', ones(1,128), fr, true);
+   [~, rf] = readDAQ('./rfdata/dataset2/tx10hz/', ones(1,128), fr, true);
    Rfc(:,:,fr) = rf.';
 end
 
 FilteredRfc = bandpass(double(Rfc), 6.6, 0.80, 40);
+%% read in AVG daq data
+[header, ~] = readDAQ('./rfdata/control/', ones(1,128), 1, true);
+
+nChannel = header(1) + 1;
+nFrame = 100;%header(2);
+nSample = header(3);
+
+Rfc = zeros(nChannel, nSample, nFrame, 'int16');
+
+for fr = 1:nFrame
+   [~, rf] = readDAQ('./rfdata/control/', ones(1,128), fr, true);
+   Rfc(:,:,fr) = rf.';
+end
+
+
+
 %%
 for i = 1:nFrame
-   imagesc(20.*log10(abs(squeeze(FilteredRfc(:,:,i)))).');
+   imagesc(20.*log10(abs(squeeze(FilteredRfc(:,:,i)))./600).');
    title(num2str(i));
    pause(0.1);
 end
@@ -28,20 +44,41 @@ end
 for i = 1:nFrame
    plot(FilteredRfc(64,:,i));
    title(num2str(i));
+   axis([250 1200 -100 100]);
    pause(0.1);
 end
+
+%%
+RfcAvg = zeros(nChannel, nSample, nFrame/10);
+for i = 1:(nFrame/10)
+   
+    RfcAvg(:,:,i) = mean(FilteredRfc(:,:,i:(i+9)),3);
+end
+%%
+for i = 1:(nFrame/10)
+   imagesc(20.*log10(abs(squeeze(RfcAvg(:,:,i)))).');
+   title(num2str(i));
+   pause(0.1);
+end
+%%
+for i = 1:(nFrame/10)
+   plot(RfcAvg(64,:,i));
+   title(num2str(i));
+   pause(0.1);
+end
+
 %%
 
 RxPos = [((0:127).*300e-6 + 150e-6 - 64*300e-6); zeros(1,128); zeros(1,128)];
 
-nCompare = 200;
-delta = 4e-6;
+nCompare = 400;
+delta = 0.1e-6;
 deltaR = -(nCompare - 1)/2*delta:delta:(nCompare - 1)/2*delta;
 
-FieldPos = [ones(1, nCompare).*0; zeros(1, nCompare); 0.0289 + deltaR];
+FieldPos = [ones(1, nCompare).*0; zeros(1, nCompare); 0.020 + deltaR];
 
 BfSigMat = gtbeamform(FilteredRfc, [], RxPos, FieldPos, 200, 'plane');
 
 VelEst = ftdoppler2(BfSigMat, FieldPos, 100);
 
-plot(VelEst);
+plot(VelEst,':.');
