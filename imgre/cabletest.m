9f5bc779981d550374ecfa7fbdeb2a56f2dc2d94
Bernie <bsauce@gmail.com>
2013-08-14 14:33:39 -0400

    -

--- imgre/cabletest.m
+++ imgre/cabletest.m
@@ -1,17 +1,90 @@
+%% ADDPATHS
+import maq.*
+import mexo.*
+addpath './bin/'
+%% TEXO OBJECT INIT
+tx = texoTransmitParams();
+rx = texoReceiveParams();
+apr = texoCurve();
 
+apr.top = 10;
+apr.mid = 50;
+apr.btm = 100;
+apr.vmid = 50;
 
-import ultrasonix.*
+tx.centerElement = 64.5;
+tx.aperture = 128; 
+tx.angle = 0;
+tx.focusDistance = 300000;
+tx.frequency = 6600000;
+tx.pulseShape = '+-';
+tx.speedOfSound = 1500;
+tx.tableIndex = -1;
+tx.useManualDelays = false;
+tx.manualDelays = zeros(1,129);
+tx.useMask = false;
+tx.mask = zeros(1,128);
+tx.sync = 1;
 
-rfc = zeros(2000, 128, 16);
-for ch = 0:15
-    dirname = ['d:/data/ch' num2str(ch) '/'];
-    [~,rf] = readDAQ(dirname, ones(1,128), 1, true);
-    rfc(:,:,ch+1) = rf;
+rx.centerElement = 64.5;
+rx.aperture = 64; 
+rx.angle = 0; 
+rx.maxApertureDepth = 50000; 
+rx.acquisitionDepth = 50000;
+rx.saveDelay = 0; 
+rx.speedOfSound = 1500;  
+rx.channelMask = [-1 -1];%[uint32(2^32) uint32(2^32)];%[uint32(0) uint32(2147483648)];  
+rx.applyFocus = false; 
+rx.useManualDelays = false; 
+rx.manualDelays = zeros(1,65); 
+rx.customLineDuration = 0; 
+rx.lgcValue = 0; 
+rx.tgcSel = 0; 
+rx.tableIndex = -1; 
+rx.decimation = 0; 
+rx.numChannels = 64; 
+rx.rxAprCrv = apr;
+rx.weightType = 1;
+rx.useCustomWindow = false;
+rx.window = zeros(1,64);
+%% TEXO INIT
+if ~texoInit('./bin/dat/', 3, 4, 0, 64, 0, 128)
+    error('texoInit failed');
 end
-%%
-for ch = 0:15
-   imagesc(rfc(:,:,ch+1), [-500 500]); 
-   title(num2str(ch));
-   colorbar;
-   pause
+texoClearTGCs();
+if ~texoAddTGCFixed(0.60)
+    error('texoAddTGCFixed failed');
 end
+texoSetSyncSignals(1,1,3);
+texoActivateProbeConnector(0);
+texoForceConnector(3);
+texoEnableSyncNotify(false);
+%% TEXO SEQUENCING
+if ~texoBeginSequence()
+    error('texoBeginSequence failed');
+end
+
+tx.centerElement = 64.5;
+rx.centerElement = 64.5;
+[success, lineSize, lineDuration] = texoAddLine(tx, rx);
+
+lineSize
+lineDuration
+if ~texoEndSequence()
+    error('texoEndSequence failed');
+end
+texoSetPower(15,15,15);
+
+%% RUN TEST
+if ~texoRunImage()
+    error('texoRunImage failed');
+end
+pause
+if ~texoStopImage()
+    error('texoStopImage failed');
+end
+texoGetCollectedFrameCount()
+
+%% SHUTDOWN
+texoShutdown()
+
