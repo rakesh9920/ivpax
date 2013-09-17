dfe7217b8261fe21208bee7373cf0b7eb65487f0
Bernie <bsauce@gmail.com>
2013-09-17 08:33:53 -0400

    -

--- imgre/+ultrasonix/cmut_imaging_script.m
+++ imgre/+ultrasonix/cmut_imaging_script.m
@@ -1,8 +1,12 @@
 %% ADDPATHS
+
 import maq.*
 import mexo.*
+import imager.*
 addpath './bin/'
+
 %% TEXO OBJECT INIT
+
 tx = texoTransmitParams();
 rx = texoReceiveParams();
 apr = texoCurve();
@@ -13,7 +17,7 @@ apr.btm = 100;
 apr.vmid = 50;
 
 tx.centerElement = 8.5;
-tx.aperture = 16; 
+tx.aperture = 16;
 tx.angle = 0;
 tx.focusDistance = 300000;
 tx.frequency = 6000000;
@@ -27,27 +31,29 @@ tx.mask = zeros(1,128);
 tx.sync = 1;
 
 rx.centerElement = 8.5;
-rx.aperture = 16; 
-rx.angle = 0; 
-rx.maxApertureDepth = 50000; 
+rx.aperture = 16;
+rx.angle = 0;
+rx.maxApertureDepth = 50000;
 rx.acquisitionDepth = 50000;
-rx.saveDelay = 0; 
-rx.speedOfSound = 1500;  
-rx.channelMask = [-1 -1];%[uint32(2^32) uint32(2^32)];%[uint32(0) uint32(2147483648)];  
-rx.applyFocus = false; 
-rx.useManualDelays = false; 
-rx.manualDelays = zeros(1,65); 
-rx.customLineDuration = 0; 
-rx.lgcValue = 0; 
-rx.tgcSel = 0; 
-rx.tableIndex = -1; 
-rx.decimation = 0; 
-rx.numChannels = 64; 
+rx.saveDelay = 0;
+rx.speedOfSound = 1500;
+rx.channelMask = [-1 -1];%[uint32(2^32) uint32(2^32)];%[uint32(0) uint32(2147483648)];
+rx.applyFocus = true;
+rx.useManualDelays = false;
+rx.manualDelays = zeros(1,65);
+rx.customLineDuration = 0;
+rx.lgcValue = 0;
+rx.tgcSel = 0;
+rx.tableIndex = -1;
+rx.decimation = 0;
+rx.numChannels = 16;
 rx.rxAprCrv = apr;
 rx.weightType = 1;
 rx.useCustomWindow = false;
 rx.window = zeros(1,64);
+
 %% TEXO INIT
+
 if ~texoInit('./bin/dat/', 3, 4, 0, 64, 0, 128)
     error('texoInit failed');
 end
@@ -59,43 +65,49 @@ texoSetSyncSignals(1,1,3);
 texoActivateProbeConnector(0);
 texoForceConnector(3);
 texoEnableSyncNotify(false);
+
 %% TEXO SEQUENCING
+
 pulseShape = '+-';
 power = 11;
-centerElement = 15;
-tx.aperture = 1; 
-txChannel = 2;
-rxChannel = 1;
 
 if ~texoBeginSequence()
     error('texoBeginSequence failed');
 end
 
-txMask = zeros(1,128);
-txMask(1:128) = 1;
-tx.mask = txMask;
 tx.pulseShape = pulseShape;
-tx.centerElement = centerElement;
-rx.centerElement = centerElement;
+tx.aperture = 16;
 
-[success, lineSize, lineDuration] = texoAddLine(tx, rx);
+for line = 1:32
+    
+    tx.centerElement = 0.5 + (line - 1)*0.5;
+    rx.centerElement = 0.5 + (line - 1)*0.5;
+    [success, lineSize, lineDuration] = texoAddLine(tx, rx);
+end
 
-lineSize
-lineDuration
 if ~texoEndSequence()
     error('texoEndSequence failed');
 end
+
 texoSetPower(15, power, power);
 
-%% RUN TEST
-if ~texoRunImage()
-    error('texoRunImage failed');
-end
-pause
-if ~texoStopImage()
-    error('texoStopImage failed');
-end
-texoGetCollectedFrameCount()
+lineSize
+lineDuration
+frameSize = texoGetFrameSize();
+samplesPerFrame = frameSize/2;
+
+%% SET IMAGING PARAMETERS
+imgprms = containers.Map();
+
+imgprms('pxwidth') = 245e-6/2;
+imgprms('label') = false;
+imgprms('cmap') = 'gray';
+
+%% COLLECT FRAMES
+texoCollectFrames(1);
+BfMat = texoGetCine(samplesPerFrame);
+
+imager(BfMat, 20, imgprms);
 
 %% SHUTDOWN
 texoShutdown()
