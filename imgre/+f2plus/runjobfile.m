b59dd67bfb29fec1476d9ef99b444619ceb12309
Bernie <bsauce@gmail.com>
2013-11-18 12:46:08 -0500

    -

--- imgre/+f2plus/runjobfile.m
+++ imgre/+f2plus/runjobfile.m
@@ -16,7 +16,7 @@ switch ext
         Log = loadmat(jobFile); 
 end
 
-TaskList = Log(Log.PROCESSED == false,:);
+TaskList = Log(Log.COMPLETE == false,:);
 nTask = height(TaskList);
 
 % create job and new task for each unprocessed file
@@ -26,10 +26,10 @@ Job = createJob(parcluster);
 for task = 1:nTask
    
     fHandle = str2func(cell2mat(TaskList{task, 'FUNCTION'}));
-    nOutputArgs = TaskList{task, 'NOUTPUTARG'};
-    InputArgs = cell(TaskList{task, 'INPUTARGS'});
+    nArgOut = TaskList{task, 'NARGOUT'};
+    ArgIn = cell(TaskList{task, 'ARGIN'});
 
-    createTask(Job, fHandle, nOutputArgs, InputArgs);
+    createTask(Job, fHandle, nArgOut, ArgIn);
 end
 
 % submit jobs
@@ -37,3 +37,16 @@ end
 
 end
 
+function TableOut = parsecsv(TableIn)
+
+	nRows = height(TableIn);
+	ArgIn = cell(nRows, 1);
+
+	for row = 1:nRows
+
+		nArgIn = TableIn{row, 'NARGIN'};
+		ArgIn{row} = {TableIn{row, 4:(4 + nArgIn - 1)}};
+	end
+
+	TableOut = table(TableIn.FUNCTION, TableIn.NARGOUT, ArgIn, TableIn.COMPLETE);
+end
