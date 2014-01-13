07436220ba0d8c5810012d9bfb61b3c74459b0d1
Bernie <bsauce@gmail.com>
2014-01-13 10:44:11 -0500

    -

--- imgre/flowscripts/psfgrid.m
+++ imgre/flowscripts/psfgrid.m
@@ -1,11 +1,11 @@
 
 import f2plus.batch_calc_multi
-import tools.saveadv tools.loadadv tools.readjoblist
+import tools.saveadv tools.loadadv tools.readjoblist tools.advdouble
 import sigproc.bandpassdir
 import beamform.batchbeamform beamform.sphericalmesh beamform.makejoblist
 import imagevis.envelope;
 
-PATH_MAIN = './data/icecfg1/psfgrid/';
+PATH_MAIN = './data/icecfg1/psfgrid2/';
 PATH_SCT = strcat(PATH_MAIN, 'sct/'); 
 PATH_RFRAW = strcat(PATH_MAIN, 'rf/'); 
 PATH_RFF = strcat(PATH_MAIN, 'rff/'); 
@@ -18,7 +18,7 @@ if ~exist(PATH_BF, 'dir'), mkdir(PATH_BF), end
 
 %% create target data
 rvg = (0.01:0.01:0.05) + 0.0035;
-tvg = 0;
+tvg = pi/4;
 pvg = degtorad(-40:20:40);
 org = [0 0 -0.0035];
 TargetPos = sphericalmesh(rvg, tvg, pvg, org, 1, 1, 1);
@@ -73,16 +73,16 @@ Angle = atan(X./Z);
 
 Env = reshape(EnvMat(152,1,:), [], 257, 1);
 RangeWeight = @(x) x^2;
-AngleWeight = @(x) 0.038952*abs(x) + 1;
+AngleWeight = @(x) 1*abs(x) + 1; %0.038952
 W = arrayfun(RangeWeight, Range).*arrayfun(AngleWeight, radtodeg(Angle));
 D = 20.*log10((Env.*W)./max(max((Env.*W)))); 
-D(D < -20) = -20;
+D(D < -30) = -30;
 
 %% mesh plot with 2D view
 figure;
 surf(X, Y, Z, D, 'EdgeColor', 'none');
-view([0 -1 0]);
-%axis([-0.5 0.5 -0.5 0.5 0 6].*1e-2);
+view([1 -1 0]);
+%axis([-5 5 -5 5 0 6].*1e-2);
 shading interp
 colorbar;
 axis image;
