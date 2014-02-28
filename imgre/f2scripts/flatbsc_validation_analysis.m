ec0a57531b86730af5f0ac1129ef8c89133c2fb3
Bernie <bsauce@gmail.com>
2014-02-28 01:58:10 -0500

    -

--- imgre/f2scripts/flatbsc_validation_analysis.m
+++ imgre/f2scripts/flatbsc_validation_analysis.m
@@ -1,30 +1,18 @@
 
-NFFT = 2^13;
-deltaF = fs/NFFT;
-Freq = (-NFFT/2-1:NFFT/2).*deltaF;
-F1 = round(3.5e6/deltaF) + NFFT/2 + 1;
-F2 = round(8.5e6/deltaF) + NFFT/2 + 1;
+PATH_FILE = './var';
+load PATH_FILE;
 
-SIG1 = ffts(Sig1, NFFT, fs);
-SIG2 = ffts(Sig2, NFFT, fs);
+focus = Prms.Focus;
+fs = Prms.SampleFrequency;
+c = Prms.SoundSpeed;
+fc = Prms.CenterFrequency;
+SR = Prms.Area;
 
-PSD1 = 2.*abs(SIG1(F1:F2)).^2;
-PSD2 = 2.*abs(SIG2(F1:F2)).^2;
-
-BSC1(:,inst) = PSD1*SR ./ (PSD2.*0.46*(2*pi)^2*focus^2*gateLength);
-k = (Freq(F1:F2).*2*pi/1540).';
-BSC2(:,inst) = PSD1.*k.^2*SR ./ (PSD2.*0.46*(2*pi)^2*focus^2*gateLength);
-
-focus = 0.02;
-fs = 100e6;
-c = 1540;
-fc = 5e6;
-SR = pi*0.005^2;
 focusTime = focus*2/c;
 gateLength = 5*c/fc;
 gateDuration = gateLength*2/c;
 gate = round((focusTime + [-gateDuration/2 gateDuration/2]).*fs) + 30;
-NFFT = 8196;
+NFFT = 2^13;
 deltaF = fs/NFFT;
 Freq = (-NFFT/2-1:NFFT/2).*deltaF;
 F1 = round(3.5e6/deltaF) + NFFT/2 + 1;
