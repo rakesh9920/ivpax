2b45d1f4d70699d7447a73a78f2e5a0b18586887
Bernie <bsauce@gmail.com>
2014-07-15 16:07:33 -0400

    -

--- python/scripts/vortical flow lumen/simple_lumen_apply_bsc.py
+++ python/scripts/vortical flow lumen/simple_lumen_apply_bsc.py
@@ -1,6 +1,6 @@
 # scripts / simple_lumen_apply_bsc.py
 
-from pyfield.field import apply_bsc
+from pyfield.field import apply_bsc, apply_wgn
 
 import h5py
 
@@ -9,6 +9,7 @@ file_path = './data/diagonal_lumen_data.h5'
 bsc_key = 'bsc/experimental/blood_hmtc8_shung'
 raw_key = 'field/rfdata/raw/fluid0'
 out_key = 'field/rfdata/blood/fluid0'
+dbw = 1
 ################################################################################
 
 if __name__ == '__main__':
@@ -18,3 +19,6 @@ if __name__ == '__main__':
 
     apply_bsc((file_path, raw_key), (file_path, out_key), bsc=bsc, write=True, 
         method='fir')
+    
+    apply_wgn((file_path, out_key), (file_path, out_key), dbw=dbw, 
+        write=True)
