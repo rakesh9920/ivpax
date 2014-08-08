7abe55dc92df09b30210533f2e32615087e1d337
Bernie <bsauce@gmail.com>
2014-08-07 23:10:11 -0400

    -

---
+++ python/mlfmm/fast_mpole_error.py
@@ -0,0 +1,47 @@
+# mlfmm / mpole_tester.py
+
+import numpy as np
+
+from mlfmm.fasttransforms import *
+from pyfield.util import distance
+from matplotlib import pyplot as plt
+
+quadrupole = np.array([[1, 1, 0], [1, -1, 0], [-1, 1, 0], [-1, -1, 0]])*0.1
+center = np.zeros((1,3))
+strengths = np.array([1, -1, -1, 1])
+f = 10000
+rho = 1000
+c = 1540
+k = 2*np.pi*f/c
+d = 200
+####
+
+r, theta, phi = np.mgrid[d:(d+1):1, 0:2*np.pi:360j, np.pi/2:np.pi/2+1:1]
+points = sph2cart(np.c_[r.ravel(), theta.ravel(), phi.ravel()])
+dist = distance(points, quadrupole)
+pres_quad_exact = np.sum(1j*k*rho*c/(4*np.pi)*np.exp(1j*k*dist)/ \
+    dist*strengths[None,:], axis=1)
+    
+####
+
+#ml_order = 15
+quad_order = 20
+kdir, weights = quadrule(quad_order)
+error = []
+
+#for quad_order in xrange(1,40):
+for ml_order in xrange(40):
+    
+    #kdir, weights = quadrule(quad_order)
+    
+    coeff_quad = ffcoeff(strengths, quadrupole, center, k, kdir)
+    pres_quad = ffeval(coeff_quad, points, center, weights, k, kdir, ml_order, 
+        rho, c)
+        
+    error.append(np.max((np.abs(pres_quad - pres_quad_exact))**2))
+
+####
+#fig1 = plt.figure()
+#plt.polar(theta.ravel(), np.abs(pres_quad_exact),'-')
+#plt.polar(theta.ravel(), np.abs(pres_quad),'.')
+#fig1.show()
--- python/mlfmm/fast_mpole_tester.py
+++ python/mlfmm/fast_mpole_tester.py
@@ -13,37 +13,38 @@ center = np.zeros((1,3))
 strengths = np.array([1, -1, -1, 1])
 f = 10000
 rho = 1000
-c = 1500
+c = 1540
 k = 2*np.pi*f/c
-ml_order = 10
+ml_order = 20
 quad_order = 25
+
 ####
 kdir, weights = quadrule(quad_order)
-coeff_mono = ffcoeff(strengths[0], monopole, center, k, kdir)
-coeff_di = ffcoeff(strengths[:2], dipole, center, k, kdir)
+#coeff_mono = ffcoeff(strengths[0], monopole, center, k, kdir)
+#coeff_di = ffcoeff(strengths[:2], dipole, center, k, kdir)
 coeff_quad = ffcoeff(strengths, quadrupole, center, k, kdir)
 
 ####
-r, theta, phi = np.mgrid[200:201:1, 0:2*np.pi:360j, np.pi/2:np.pi/2+1:1]
+r, theta, phi = np.mgrid[1000:1001:1, 0:2*np.pi:360j, np.pi/2:np.pi/2+1:1]
 
 points = sph2cart(np.c_[r.ravel(), theta.ravel(), phi.ravel()])
 
-pres_mono = ffeval(coeff_mono, points, center, weights, k, kdir, ml_order, rho, 
-    c)
-pres_di = ffeval(coeff_di, points, center, weights, k, kdir, ml_order, rho, 
-    c)
+#pres_mono = ffeval(coeff_mono, points, center, weights, k, kdir, ml_order, rho, 
+#    c)
+#pres_di = ffeval(coeff_di, points, center, weights, k, kdir, ml_order, rho, 
+#    c)
 pres_quad = ffeval(coeff_quad, points, center, weights, k, kdir, ml_order, rho, 
     c)
     
 ####
-dist = distance(points, monopole[None,:])
-pres_mono_exact = np.sum(1j*k*rho*c/(4*np.pi)*np.exp(1j*k*dist)/ \
-    dist*strengths[0], axis=1)
-
-dist = distance(points, dipole)
-pres_di_exact = np.sum(1j*k*rho*c/(4*np.pi)*np.exp(1j*k*dist)/ \
-    dist*strengths[None,:2], axis=1)
-    
+#dist = distance(points, monopole[None,:])
+#pres_mono_exact = np.sum(1j*k*rho*c/(4*np.pi)*np.exp(1j*k*dist)/ \
+#    dist*strengths[0], axis=1)
+#
+#dist = distance(points, dipole)
+#pres_di_exact = np.sum(1j*k*rho*c/(4*np.pi)*np.exp(1j*k*dist)/ \
+#    dist*strengths[None,:2], axis=1)
+#    
 dist = distance(points, quadrupole)
 pres_quad_exact = np.sum(1j*k*rho*c/(4*np.pi)*np.exp(1j*k*dist)/ \
     dist*strengths[None,:], axis=1)
@@ -60,6 +61,6 @@ pres_quad_exact = np.sum(1j*k*rho*c/(4*np.pi)*np.exp(1j*k*dist)/ \
 #fig3.show()
 
 fig1 = plt.figure()
-plt.polar(theta.ravel(), np.abs(pres_quad_exact),'-')
-plt.polar(theta.ravel(), np.abs(pres_quad),'.')
+plt.plot(theta.ravel(), np.abs(pres_quad_exact),'-')
+plt.plot(theta.ravel(), np.abs(pres_quad),'.')
 fig1.show()
