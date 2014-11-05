cb8d4c7561ba2f811ee0f1b15344a8a4329e477a
Bernie <bsauce@gmail.com>
2014-11-05 06:24:35 -0500

    -

--- python/scripts/bem_fmm_comparison.py
+++ python/scripts/bem_fmm_comparison.py
@@ -14,7 +14,8 @@ origin = np.array([0.0, 0.0, 0.0])
 D0 = 0.001
 dim = np.array([1.0, 1.0])*D0
 matfilename = './data/bem_vars.mat'
-
+f = 19e6
+      
 if __name__ == '__main__':
     
     matfile = loadmat(matfilename)
@@ -22,7 +23,7 @@ if __name__ == '__main__':
     s_n = np.float(matfile['dx']*matfile['dy'])
     c = np.float(matfile['fluid_c'])
     rho = np.float(matfile['fluid_rho'])
-    omega = np.float(matfile['w']  )
+    #omega = np.float(matfile['w'])
           
     nodes = matfile['node_x']
     nnodes = nodes.shape[0]
@@ -34,20 +35,21 @@ if __name__ == '__main__':
     M = np.squeeze(np.array(matfile['M'], dtype='cfloat'))
     B = np.squeeze(np.array(matfile['B'], dtype='cfloat'))
     P = np.squeeze(np.array(matfile['P'], dtype='cfloat'))
-    X = np.squeeze(np.array(matfile['U'], dtype='cfloat'))
-    Zr = np.array(matfile['Zr'], dtype='cfloat')
+    #X = np.squeeze(np.array(matfile['U'], dtype='cfloat'))
+    #Zr = np.array(matfile['Zr'], dtype='cfloat')
     membranes = np.squeeze(matfile['membranes'])
     
     del matfile
     
-    f = omega/(2*np.pi)
+    omega = 2*np.pi*f
     k = 2*np.pi*f/c
+    a_eff = np.sqrt(s_n/np.pi)
     
     op = CachedOperator()
     op.params['density'] = rho
     op.params['sound_speed'] = c
     op.params['node_area'] = s_n
-    op.params['wave_number'] = 2*np.pi*f/c
+    op.params['wave_number'] = k
     op.params['box_dims'] = dim
     op.params['origin'] = origin
     op.params['nodes'] = nodes
@@ -57,19 +59,26 @@ if __name__ == '__main__':
     op.setup(verbose=False)
     op.precompute()
 
+    # create exact acoutic impedance matrix
+    dist = distance(nodes, nodes)
+    Zr = 1j*omega*rho*s_n/(2*np.pi)*np.exp(-1j*k*dist)/dist
+    Zr[np.eye(*dist.shape).astype(bool)] = rho*c*(0.5*(k*a_eff)**2 +
+        1j*8/(3*np.pi)*k*a_eff)
+    
+    # create matrices for mechanical dynamics and full dynamics
+    Gmech = -omega**2*M + 1j*omega*B + K
+    G = Gmech + 1j*omega*Zr
+    
+    # find inverse of Gmech for use as a preconditioner
+    Gmech_inv = np.linalg.inv(Gmech)
+
+    # define matrix-vector products that use fmm
     counter = 0
-    def matvec(x):
-        
+    def matvec1(x):
         global counter
         
         u = 1j*omega*x
-        
         p1 = op.apply(2*np.squeeze(u))
-        
-        #x = np.matrix(u.reshape((-1,1)))/(1j*omega)
-        #p2 = np.array((-omega**2*M + 1j*omega*B + K)*x)
-        
-        #x = u.reshape((-1,1))/(1j*omega)
         p2 = (-omega**2*M + 1j*omega*B + K).dot(x)
         
         print counter
@@ -77,69 +86,81 @@ if __name__ == '__main__':
         
         return p1 + np.squeeze(np.array(p2))
     
-    operator = ssl.LinearOperator(shape=(1444,1444), matvec=matvec, dtype='cfloat')
-    
-    G = -omega**2*M + 1j*omega*B + K + 1j*omega*Zr
-    u = 2*1j*omega*X
-    a_eff = np.sqrt(s_n/np.pi)
-    
-    #p1 = 1j*omega*Zr.dot(X)
-    #p2 = op.apply(u)
-    #p3 = directeval(s_n*u, nodes, nodes, k, rho, c) 
-    #p3 += (rho*c*(0.5*(k*a_eff)**2 + 1j*8/(3*np.pi)*k*a_eff))/2*u
-    #error = np.abs(np.abs(p2) - np.abs(p1))/np.abs(p1)*100
-    #
-    #pp.plot(np.abs(p1))
-    #pp.plot(np.abs(p2),'.')
-    ##pp.plot(np.abs(p3),'.')
-    #pp.show()
-    
-    #Xmat = np.matrix(X.reshape((-1,1)))
-    #pres_exact = np.squeeze(np.array(G*Xmat))
-    #pres_fmm = matvec(X*1j*omega)
-    #error = np.abs(pres_fmm - pres_exact)/np.abs(pres_exact)*100
-    #
-    #pp.plot(pres_exact)
-    #pp.plot(pres_fmm, '--')
-    #pp.plot(error)
-    
-    #P = np.ones(1444, dtype='complex')
-    #counter = 0
-    res = ssl.cg(operator, P, x0=np.zeros(1444)*1e-8, maxiter=1000)
-    x = res[0]
-    
-    res2 = ssl.cg(G, P, x0=np.zeros(1444), tol=1e-12)
-    x2 = res2[0]
-    error = np.abs(np.abs(x2) - np.abs(X))/np.abs(X)*100
-    #
-    #X2 = sp.linalg.inv(np.array(G)).dot(P)
-    #u = np.zeros(nnodes, dtype='cfloat')
-    #u[P.ravel().astype(bool)] = 1
-    #umat = np.matrix(u.reshape((-1,1)))
-    #
-    #pres_exact = Zr*umat
-    #pres, _ = matvec(u)
-    
-    #q = s_n*u
-    #pres_direct = directeval(2*q, nodes, nodes, k, rho, c)
-    
-    #fig1 = pp.figure()
-    #ax1 = fig1.add_subplot(111)
-    ##ax1 = fig1.add_subplot(111, projection='3d')
-    #xmax = np.max(np.abs(x))
-    #for mem in membranes:
-    #    
-    #    membrane_nodes = mem['node_x'][0,0].astype(float)
-    #    nodesx = membrane_nodes[:,0].reshape((19,19))
-    #    nodesy = membrane_nodes[:,1].reshape((19,19))
-    #    idx = mem['node_no'][0,0].astype(int) - 1
-    #    disp = x[idx].reshape((19,19))
-    #    ax1.pcolormesh(nodesx, nodesy, np.abs(disp), vmax=xmax, vmin=0)
-    #    #ax1.plot_surface(nodesx, nodesy, np.abs(disp))
-    #
-    #ax1.set_aspect('equal')
-    #pp.show()
+    def matvec2(x):
+        global counter
         
+        u = 1j*omega*x
+        p1 = op.apply(2*np.squeeze(u))
+        p2 = Gmech_inv.dot(p1) + x
         
+        print counter
+        counter += 1
+        
+        return p2
     
+    # define linear operators
+    operator1 = ssl.LinearOperator(shape=(nnodes,nnodes), matvec=matvec1, 
+        dtype='cfloat')
+    operator2 = ssl.LinearOperator(shape=(nnodes,nnodes), matvec=matvec2, 
+        dtype='cfloat')
+        
+    x_fmm, niter = ssl.cgs(operator2, Gmech_inv.dot(P), x0=np.zeros(1444), 
+        tol=1e-9, maxiter=20)
+    #x_fmm, niter = ssl.cgs(operator1, P, M=Gmech_inv, x0=np.zeros(1444), 
+        #tol=1e-9, maxiter=15)
+    x_exact = sp.linalg.solve(G, P)
+    #x_exact = sp.linalg.inv(G).dot(P)
+    error = np.abs(np.abs(x_fmm) - np.abs(x_exact))/np.abs(x_exact)*100
+    
+    p_fmm = op.apply(2*1j*omega*x_exact)
+    p_exact = G.dot(x_exact)
+    
+    fig1 = pp.figure()
+    ax1 = fig1.add_subplot(111)
+    #ax1 = fig1.add_subplot(111, projection='3d')
+    xmax = np.max(np.abs(x_fmm))
+    xmin = np.min(np.abs(x_fmm))
+    for mem in membranes:
+        
+        membrane_nodes = mem['node_x'][0,0].astype(float)
+        center = np.squeeze(np.array(mem['center'][0,0].astype(float)))
+        width = np.squeeze(np.array(mem['width'][0,0].astype(float)))
+        corner1 = center - width/2.0
+        corner2 = center + width/2.0
+        #nodesx = membrane_nodes[:,0].reshape((19,19))
+        #nodesy = membrane_nodes[:,1].reshape((19,19))
+        idx = mem['node_no'][0,0].astype(int) - 1
+        knodes = idx.size
+        nodesx, nodesy = np.mgrid[corner1[0]:corner2[0]:20*1j,corner1[1]:\
+            corner2[1]:20*1j] - (width/19/2)[:,None,None]
+        disp = x_fmm[idx].reshape((19,19))
+        pc = ax1.pcolormesh(nodesx, nodesy, np.abs(disp), vmax=xmax, vmin=0, 
+            cmap='jet')
+        #ax1.plot_surface(nodesx, nodesy, np.abs(disp), vmax=xmax, vmin=xmin,
+            #rstride=1, cstride=1)
+    
+    pp.colorbar(pc)
+    ax1.set_aspect('equal')
+    
+    
+    
+    fig2 = pp.figure()
+    ax2 = fig2.add_subplot(111)
+    ax2.plot(np.abs(x_exact))
+    ax2.plot(np.abs(x_fmm),'r.', markersize=4)
+    ax2.legend(('direct','mlfmm + cgs'), loc='best', numpoints=1)
+    ax2.set_xlabel('Node no.')
+    ax2.set_ylabel('Displacement amplitude (m)')
+    ax2.set_title('Displacement profile for f = %2.2f MHz' % (f/1e6))
+        
+    pp.show()
+
+    fig3 = pp.figure()
+    ax3 = fig3.add_subplot(111)
+    ax3.plot(error)
+    ax3.set_xlabel('Node no.')
+    ax3.set_ylabel('Error (%)')
+    ax3.set_title('Displacement error for f = %2.2f MHz' % (f/1e6))
+        
+    pp.show()
     
